0806e0c1f72bd82d2029fd914ac68e6e351a9ee1
SharedPreferences <boothaganesh.jdev@gmail.com>
2021-09-23 13:02:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import Webcam from "react-webcam";
+import { useRef } from "react";
+import { DialogTitle, Dialog, Button, TextField } from "@material-ui/core";
 function App() {
+  const [image, setImage] = useState({
+    image:
+      "https://image.freepik.com/free-vector/cardboard-box-opened-isolated-cartoon-style_1308-49807.jpg",
+    name: "",
+  });
+  const WebRef = useRef(null);
+  const [openCapture, setopenCapture] = useState(false);
+  const openPop = () => {
+    setopenCapture(true);
+  };
+  const closeCapture = () => {
+    setopenCapture(false);
+  };
+
+  const storeImage = () => {
+    console.log(WebRef.current.getScreenshot());
+    let picBase64 = WebRef.current.getScreenshot();
+
+    setImage({ ...image, image: picBase64, name: "Sample_IMG" });
+
+    setopenCapture(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div
+        style={{
+          display: "flex",
+          padding: "1%",
+          width: "30%",
+          border: "2px solid red",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <TextField label="Capture" value={image.name} />
+        <Button variant="contained" onClick={openPop}>
+          Capture
+        </Button>
+        <img style={{ width: "20%", height: "20%" }} src={image.image} />
+      </div>
+
+      <div style={{ margin: "1%" }}>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            border: "2px solid white",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Dialog open={openCapture} onClose={closeCapture}>
+            <DialogTitle>{"Choose Your Pattern"}</DialogTitle>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                width: "100%",
+                flexDirection: "column",
+              }}
+            >
+              <Webcam style={{ width: "100%", height: "35%" }} ref={WebRef} />
+              <Button variant="contained" onClick={storeImage}>
+                Capture
+              </Button>
+            </div>
+          </Dialog>
+        </div>
+      </div>
     </div>
   );
 }
